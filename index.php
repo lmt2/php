abebf9f9688e798069d70c7f7ad18781f1bafa53
LMT <Tamas.Lang.Miticzky@gmail.com>
2020-09-24 15:07:57 +0200

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+            $a=1;
+            $b=2;
+            $c=3;
+
+            echo "Hello World!";
+
+        ?>
+    </body>
+</html>
